35bfe8634180a2f1581fbbd6ab3712997ea4311e
Bagheera-e46 <andjelkamladenovic8@gmail.com>
2020-07-29 22:21:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from "./logo.svg";
+import NewContact from "./components/new-contact/NewContact";
+import PhoneDirectory from "./components/phone-directory/PhoneDirectory";
+import DirectoryItem from "./model/DirectoryItem";
+import Directory from "./model/Directory";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.directory = new Directory();
+
+        this.state = {
+            contacts: this.directory.contacts
+        }
+
+        this.createContact = this.createContact.bind(this);
+        this.changeContactState = this.changeContactState.bind(this);
+        this.deleteContact = this.deleteContact.bind(this);
+    }
+
+    createContact(contentName, contentPhone) {
+        let contact = new DirectoryItem(contentName, contentPhone, new Date());
+        let contacts = this.directory.add(contact);
+
+        this.setState({ contacts: contacts });
+    }
+
+    changeContactState(contact) {
+        let contacts = this.directory.changeState(contact);
+        this.setState({ contacts: contacts });
+    }
+
+    deleteContact(contact) {
+        let contacts = this.directory.delete(contact);
+        this.setState({ contacts: contacts });
+    }
+
+    render() {
+        return (<div id="app" >
+            <img src={logo} className="App-logo" alt="logo" />
+            <NewContact createContact={this.createContact}></NewContact>
+            <PhoneDirectory contacts={this.state.contacts} onDeleteContact={this.deleteContact}></PhoneDirectory>
+        </div>)
+    }
+
 }
 
 export default App;
